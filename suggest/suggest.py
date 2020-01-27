4e1642c24d1072aae062d50da93b1a344f39f73a
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-01-27 15:44:27 -0500

    Update suggest.py

--- suggest/suggest.py
+++ suggest/suggest.py
@@ -44,7 +44,7 @@ class Suggest(commands.Cog):
                 embed=discord.Embed(title=suggestion, color=self.bot.main_color)
                 embed.set_author(name=f"Suggestion by {ctx.author}:", icon_url=ctx.author.avatar_url)
                 await suggestion_channel.send(embed=embed)
-                await self.bot.get_channel(int(config["suggestion-channel"]["channel"].add_reaction('\N{WHITE HEAVY CHECK MARK}')
+                await suggestion_channel.embed.add_reaction('\N{WHITE HEAVY CHECK MARK}')
                 await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
 
 def setup(bot):
