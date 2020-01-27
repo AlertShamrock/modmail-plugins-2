3e51c8e04f4226bfccb8d1cbdbb467d93181ef06
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-01-27 15:12:44 -0500

    Update suggest.py

--- suggest/suggest.py
+++ suggest/suggest.py
@@ -45,7 +45,7 @@ class Suggest(commands.Cog):
                 embed=discord.Embed(title=suggestion, color=self.bot.main_color)
                 embed.set_author(name=f"Suggestion by {ctx.author}:", icon_url=ctx.author.avatar_url)
                 await suggestion_channel.send(embed=embed)
-                await embed.message.add_reaction('✅')
+                await ctx.embed.add_reaction('✅')
 
 def setup(bot):
     bot.add_cog(Suggest(bot))
