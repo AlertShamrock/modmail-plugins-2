ec8cc0e6de576d94f4d7e9d00f47d1b9ebb3ceac
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-01-27 19:31:17 -0500

    Update react-on-ping.py

--- react-on-ping/react-on-ping.py
+++ react-on-ping/react-on-ping.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class ReactOnPing(commands.Cog):
     """Reacts with a ping emoji when someone gets pinged."""
     emojis = [
-    "shamrock:
+
     ]
 
     def __init__(self, bot):
@@ -14,8 +14,6 @@ class ReactOnPing(commands.Cog):
     async def on_message(self, message):
         if '<@412762426132398102>' in message.content.upper():
             ctrx.channel.send("If you need help please DM me! Please do not PING/MENTION staff")
-            for emoji in self.emojis:
-                await message.add_reaction(emoji)
 
 def setup(bot):
     bot.add_cog(ReactOnPing(bot))
