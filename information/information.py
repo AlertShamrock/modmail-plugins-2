b34ce7ef8b7220ec1b485408de632b01b6186ad8
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-02-01 12:10:59 -0500

    Update information.py

--- information/information.py
+++ information/information.py
@@ -21,14 +21,9 @@ class Information(Cog):
         embed = discord.Embed(
             title="__**Commands**__",
             color=self.bot.main_color,
-            description="__Say ***THESE*** commands in ***THIS*** channel **ONLY!**__",
+            description="__**Update your Discord rank**__ /n Say this command `!getroles`",
         )
         embed.add_field(name="__Update your Discord rank__",value="**Command** \n `!getroles`", inline=false)
-        embed.add_field(name="__Report__",value="Reporting a Discord User! **Command** `?report <Discord User> <Reason>`", inline=false)
-        ##embed.add_field(name="__Inactivity notice__",value="Going on vacation, or not going be on base? \n **Say these commands** \n
-##`!apply`\n **NOTE:** Can be demoted until HQ (Head Quarters) approves of your inactivity request.", inline=false)
-      ##  embed.add_field(name="__Suggestions__",value="Want to suggest something to Army discord or on to a base? \n **Say these commands** \n **Add these before you MESSAGE** \n ***Message Prefix*** \n `[BASE]` \n `[DISCORD]` \n `[GROUP]` \n **Command:** \n`!suggest (PREFIX) (message)`")
-        embed.add_field(name="__Quote__",value="Add a quote onto quotes channel! **Command** `!quote <Quote here>`", inline=false)
         embed.set_thumbnail(url=ctx.guild.icon_url)
         await ctx.send(embed=embed)
 
